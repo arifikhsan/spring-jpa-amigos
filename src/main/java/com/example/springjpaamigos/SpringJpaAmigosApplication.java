deb75f1a0cbb4f9e51b3dce08c0d0b74cc21ee2f
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-03-07 13:35:37 +0700

    Initial commit

---
+++ src/main/java/com/example/springjpaamigos/SpringJpaAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpaamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAmigosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaAmigosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjpaamigos/SpringJpaAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpaamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAmigosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
